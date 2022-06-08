e504d257e9343c03c73fa72636731bfeef298968
josuembuyu6 <josuembuyu94@gmail.com>
2022-06-08 10:36:12 +0100

    Edit and delete user

--- src/features/users/EditUser.js
+++ src/features/users/EditUser.js
@@ -1,17 +1,29 @@
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate, useParams } from "react-router-dom"
 import Button from "../../components/Button"
 import TextField from "../../components/TextField"
+import { editUser } from "./userSlice"
 
 const EditUser = () => {
+  const params = useParams();
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const users = useSelector(store => store.users);
+  const existingUser = users.filter(user => user.id === params.id);
+  const { name, email } = existingUser[0];
   const [values, setValues] = useState({
-    name: '',
-    email: ''
+    name,
+    email
   });
 
   const handleEditUser = () => {
     setValues({ name: '', email: '' });
+    dispatch(editUser({
+      id: params.id,
+      name: values.name,
+      email: values.email
+    }))
     navigate('/');
   }
 
